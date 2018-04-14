645a39ea042dc5f8708212387b5ddd8ffd1dc0fb
Jon Wheeler <wheelibin@gmail.com>
2018-04-14 21:25:25 +0100

    Enable redux devtools

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { rootReducer } from "./modules/root";
 
-//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
